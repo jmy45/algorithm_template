f10fe23f503a546437e1cf22b8d68ce953d16ea8
jmy45 <996005918@qq.com>
2022-01-05 10:12:59 +0800

    master

---
+++ strings/Trie2.cpp
@@ -0,0 +1,54 @@
+/*
+5
+I abc
+Q abc
+Q ab
+I ab
+Q ab
+*/
+//这个作为模板2吧，可能在使用时需要修改下。
+#include<bits/stdc++.h>
+using namespace std;
+class Trie{
+    vector<Trie*> children;
+    int n;
+public:
+    Trie():children(26,nullptr),n(0){
+
+    };
+    void Insert(string &str){
+        Trie* root=this;
+        for (char&ch:str){
+            if (!root->children[ch-'a'])
+                root->children[ch-'a']=new Trie();
+            root=root->children[ch-'a'];
+        }
+        root->n++;
+    }
+    int Find(string &str){
+        Trie* root=this;
+        for (char&ch:str){
+            if (!root->children[ch-'a'])
+                return 0;
+            root=root->children[ch-'a'];
+        }
+        return root->n;
+    }
+};
+int N;
+int main(){
+    cin>>N;
+    Trie* root=new Trie();
+    for (int i=0;i<N;i++){
+        char c;
+        cin>>c;
+        cin.get();
+        string s;
+        cin>>s;
+        if (c=='I')
+            root->Insert(s);
+        else
+            cout<<root->Find(s)<<endl;
+    }
+    return 0;
+}
